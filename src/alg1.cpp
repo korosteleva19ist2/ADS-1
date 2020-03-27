4000f6bc603fe1de7d23b84ce6afcc3115d0d875
korosteleva19ist2 <62651383+korosteleva19ist2@users.noreply.github.com>
2020-03-27 20:29:12 +0300

    Create alg1.cpp

---
+++ src/alg1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int cbinsearch(int* arr, int size, int value)
+{
+int j = size - 1;
+int count = 0;
+for (int i = 0; i < j; i++)
+{
+int mid = (i + j) / 2;
+if (arr[mid] == value)
+{
+count++;
+}
+else if (value <= arr[mid])
+j = mid;
+else
+j = mid + 1;
+}
+return count;
+}
+int main()
+{
+const int size = 10;
+int arr[size] = { 5,2,5,5,5,5,5,8,9,0 };
+cout<< cbinsearch(arr, size, 5) <<endl;
+return 0;
+}
